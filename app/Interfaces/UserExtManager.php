5e3f9f2cc329e4f59b808008bf7e9af7b1dd0928
Michal Voců <michal.vocu@gmail.com>
2018-10-19 17:58:20 +0200

    work

--- app/Interfaces/UserExtManager.php
+++ app/Interfaces/UserExtManager.php
@@ -2,11 +2,14 @@
 
 namespace App\Interfaces;
 
+use App\Models\Database\ExtSource;
 use App\Models\Database\UserExt;
 
 interface UserExtManager
 {
     public function extractUserWithAttributes(UserExt $user_ext) : array;
   
+    public function createUserWithAttributes(ExtSource $source, array $data) : UserExt;
+    
 }
 
--- app/Services/UserExtManager.php
+++ app/Services/UserExtManager.php
@@ -3,7 +3,11 @@
 namespace App\Services;
 
 use App\Models\Database\Contact;
+use App\Models\Database\ExtSource;
+use App\Models\Database\UserExt;
+use Illuminate\Support\Facades\DB;
 use App\Interfaces\UserExtManager as UserExtManagerInterface;
+use App\Models\Database\UserExtAttribute;
 
 class UserExtManager implements UserExtManagerInterface
 {
@@ -37,6 +41,41 @@ class UserExtManager implements UserExtManagerInterface
         return $result;
     }
 
+    /**
+     * {@inheritDoc}
+     * @see \App\Interfaces\UserExtManager::createUserWithAttributes()
+     */
+    public function createUserWithAttributes(ExtSource $source, array $data): UserExt
+    {
+        $user = new UserExt();
+        $user->fill($data);
+        $user->extSource()->associate($source);
+        $attrDefs = array();
         
+        foreach($source->attributes as $attrDef) {
+            $attrDefs[$attrDef->name] = $attrDef;    
+        }
+        
+        DB::transaction(function() use ($source, $data, $user, $attrDefs) {
+            
+            $user->save();
+            
+            if(array_key_exists('attributes', $data) && is_array($data['attributes'])) {
+                foreach($data['attributes'] as $name => $value) {
+                    if(array_key_exists($name, $attrDefs)) {
+                        $attrDef = $attrDefs[$name];    
+                        $attr = new UserExtAttribute(['value' => $value]);
+                        $attr->attrDesc()->associate($attrDef);
+                        $user->attributes()->save($attr);
+                    }
+                }
+            }
+                
+        });
+        return $user; 
+    }
+
+
+            
 }
 
