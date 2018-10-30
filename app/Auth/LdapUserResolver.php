cd060ed4959835ed2fd1810947879734854daa13
Michal Voců <michal.vocu@gmail.com>
2018-10-30 07:07:59 +0100

    use phone and mail for user resolving

--- app/Auth/LdapUserResolver.php
+++ app/Auth/LdapUserResolver.php
@@ -2,6 +2,7 @@
 namespace App\Auth;
 
 use Adldap\Laravel\Resolvers\UserResolver;
+use App\Models\Database\Contact;
 
 class LdapUserResolver extends UserResolver
 {
@@ -9,6 +10,36 @@ class LdapUserResolver extends UserResolver
     {
         return $this->query()->findByDn($identifier);
     }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Adldap\Laravel\Resolvers\UserResolver::byCredentials()
+     */
+    public function byCredentials(array $credentials = array())
+    {
+        if (empty($credentials)) {
+            return;
+        }
+        
+        if(null != $user = parent::byCredentials()) 
+            return $user;
+        
+        $username = $credentials[$this->getLdapDiscoveryAttribute()];
+        // normalize input through database model mutators
+        $contact = new Contact(['mail' => $username, 'phone' => $username]);
+        $mail = $contact->mail;
+        $phone = $contact->phone;
+        $query = $this->query();
+        if(!empty($phone)) 
+            $query = $query->orWhereEquals('telephoneNumber', $phone);
+        if(!empty($mail)) 
+            $query = $query->orWhereEquals('mail', $mail);
+        if(!empty($phone) || !empty($mail)) 
+            return $query->first();
+        
+        return;
+    }
+
 
 }
 
