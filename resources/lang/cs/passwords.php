fb4a1acb2387f02a47a0ebb0ad8078ca70636ab5
Michal Vocu <michal@oinochos.mestouvaly.local>
2018-11-21 13:26:23 +0100

    add translation

--- resources/lang/cs/passwords.php
+++ resources/lang/cs/passwords.php
@@ -14,7 +14,8 @@ return [
 
     'password' => 'Heslo musí obsahovat alespoň 6 znaků a musí se shodovat s ověřením.',
     'reset'    => 'Heslo bylo obnoveno!',
-    'sent'     => 'E-mail s instrukcemi k obnovení hesla byl odeslán!',
+    'sent'     => 'E-mail s instrukcemi k obnovení hesla byl odeslán.',
+    'sent.sms' => 'SMS s klíčem pro obnovu hesla byla odeslána.',
     'token'    => 'Klíč pro obnovu hesla je nesprávný.',
     'user'     => 'Nepodařilo se najít uživatele s touto e-mailovou adresou.',
 ];
