50332e5104cd6864afc86fbf4b7899a427521690
Michal Voců <michal.vocu@gmail.com>
2019-01-30 17:36:53 +0100

    work

--- app/Services/SynchronizationManager.php
+++ app/Services/SynchronizationManager.php
@@ -4,13 +4,31 @@ namespace App\Services;
 
 use App\Interfaces\SynchronizationManager as SynchronizationManagerInterface;
 use App\Models\Database\ExtSource;
+use App\Interfaces\ExtSourceManager;
+use App\Interfaces\UserExtManager;
 
 class SynchronizationManager implements SynchronizationManagerInterface
 {
 
+    protected $ext_source_mgr;
+    protected $user_ext_mgr;
+    
+    
+    public function __construct(ExtSourceManager $ext_source_mgr,
+                                UserExtManager $user_ext_mgr) 
+    {
+        $this->ext_source_mgr = $ext_source_mgr;
+        $this->user_ext_mgr = $user_ext_mgr;
+    }
+    
     public function synchronizeExtSource(ExtSource $es)
     {
-        
+        $connector = $this->ext_source_mgr->getConnector($es);
+        if($connector->supportsUserListing($es)) {
+            $users = $connector->listUsers($es);
+            return $this->user_ext_mgr->syncUsers($es, $users);
+        }
+        return null;
     }
     
 }
