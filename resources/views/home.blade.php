c5cf54def3191f4cc2582f173cff32ea46a217eb
Michal Vocu <michal@oinochos.mestouvaly.local>
2019-03-21 13:38:13 +0100

    work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">{{ __('Dashboard') }} @if (isset($user)) - {{ $user->name }} @endif</div>
 
@@ -16,24 +16,24 @@
 
                     @if ( isset($user) )
                     	<div class="row">
-                    		<div class="col-xs-4 text-right">{{ __('Login') }}</div>
-                    		<div class="col-xs-8">{{ implode(",", $user->uid) }}</div>
+                    		<div class="col-xs-3 text-right">{{ __('Login') }}</div>
+                    		<div class="col-xs-9">{{ implode(",", $user->uid) }}</div>
                     	</div>
                     	<div class="row">
-                    		<div class="col-xs-4 text-right">{{ __('E-mail') }}</div>
-                    		<div class="col-xs-8">{{ empty($user->getEmail()) ? "" : implode(";", $user->mail) }}</div>
+                    		<div class="col-xs-3 text-right">{{ __('E-mail') }}</div>
+                    		<div class="col-xs-9">{{ empty($user->getEmail()) ? "" : implode(";", $user->mail) }}</div>
                     	</div>
                     	<div class="row">
-                    		<div class="col-xs-4 text-right">{{ __('Phone') }}</div>
-                    		<div class="col-xs-8">{{ empty($user->getTelephoneNumber()) ? "" : implode("; ", $user->telephonenumber) }}</div>
+                    		<div class="col-xs-3 text-right">{{ __('Phone') }}</div>
+                    		<div class="col-xs-9">{{ empty($user->getTelephoneNumber()) ? "" : implode("; ", $user->telephonenumber) }}</div>
                     	</div>
 					@foreach ($accounts as $account)
                         <div class="row">
-                                <div class="col-xs-4 text-right">{{ __($account['name']) }}</div>
-                                <div class="col-xs-2">{{ empty($value = $user->getFirstAttribute('employeenumber;x-'.$account['tag'])) ? "ne" : "ano" }}</div>
+                                <div class="col-xs-3 text-right">{{ __($account['name']) }}</div>
+                                <div class="col-xs-1">{{ empty($value = $user->getFirstAttribute('employeenumber;x-'.$account['tag'])) ? "ne" : "ano" }}</div>
 					@if (!empty($user->getFirstAttribute('employeenumber;x-'.$account['tag']))) 
-                                <div class="col-xs-3">tel. {{ empty($account['phone']) ? "" : $account['phone'] }}</div>
-                                <div class="col-xs-3">mail {{ empty($account['email']) ? "" : $account['email'] }}</div>
+                                <div class="col-xs-4">tel. {{ empty($account['phone']) ? "" : $account['phone'] }}</div>
+                                <div class="col-xs-4">mail: {{ empty($account['email']) ? "" : $account['email'] }}</div>
 					@endif
                         </div>
 					@endforeach
