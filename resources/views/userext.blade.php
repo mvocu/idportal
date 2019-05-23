caf50d1185837df8c380ba2f7e7a368303cd2d72
Michal Voců <michal.vocu@gmail.com>
2019-05-23 12:53:41 +0200

    adapt to 5.5

--- resources/views/userext.blade.php
+++ resources/views/userext.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __($ext_source->name) }} - {{ __('User detail') }}</div>
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">{{ __($ext_source->name) }} - {{ __('User detail') }}</div>
 
-                <div class="card-body">
+                <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -21,17 +21,17 @@
                     @endif
 
                     <form method="POST" action="{{ route('ext.account.modify', [ 'user_ext' => $user_ext, 'action' => $action ]) }}" aria-label="{{ __('Update contacts') }}">
-                        @csrf
+                        {{ csrf_field() }}
 
 						@foreach ($editable as $attrdef)
-                        <div class="form-group row">
-                            <label for="{{ $attrdef->name }}" class="col-md-4 col-form-label text-md-right">{{ $attrdef->display_name }}</label>
+                        <div class="form-group row{{ $errors->has($attrdef->name) ? ' has-error' : '' }}">
+                            <label for="{{ $attrdef->name }}" class="col-md-4 control-label col-form-label text-right">{{ $attrdef->display_name }}</label>
 
                             <div class="col-md-6">
-                                <input id="{{ $attrdef->name }}" type="text" class="form-control{{ $errors->has($attrdef->name) ? ' is-invalid' : '' }}" name="{{ $attrdef->name }}" value="{{ old($attrdef->name) }}" required>
+                                <input id="{{ $attrdef->name }}" type="text" class="form-control" name="{{ $attrdef->name }}" value="{{ old($attrdef->name) }}" required>
 
                                 @if ($errors->has($attrdef->name))
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="help-block" role="alert">
                                         <strong>{{ $errors->first($attrdef->name) }}</strong>
                                     </span>
                                 @endif
@@ -41,7 +41,7 @@
                         
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Submit') }} 
                                 </button>
@@ -51,22 +51,22 @@
 
               	</div>
               	
-              	<div class="card-body">
-                        <div class="form-group row border-bottom">
-                                <div class="col-4 text-right font-weight-bold">{{ __('Attribute') }}</div>
-                                <div class="col-8 font-weight-bold">{{ __('Value') }}</div>
+              	<div class="panel-body">
+                        <div class="form-group row border-bottom" style="border-bottom: 1px solid lightgrey">
+                                <div class="col-md-4 text-right font-weight-bold">{{ __('Attribute') }}</div>
+                                <div class="col-md-8 font-weight-bold">{{ __('Value') }}</div>
 						</div>						
 					@foreach ($attributes as $name => $value)
                         <div class="row">
-                                <div class="col-4 text-right">{{ __($name) }}</div>
-                                <div class="col-8">{{ $value }}</div>
+                                <div class="col-md-4 text-right">{{ __($name) }}</div>
+                                <div class="col-md-8">{{ $value }}</div>
 						</div>						
 					@endforeach
               	</div>
 
-            	<div class="card-footer">
+            	<div class="panel-footer">
 					<div class="row">
-						<div class="col-2"><a href="{{ route('home') }}" class="btn btn-default btn-block"><span class="fa fa-long-arrow-left">&nbsp;</span>{{ __('Back') }}</a></div>
+						<div class="col-md-2"><a href="{{ route('home') }}" class="btn btn-default btn-block"><span class="fa fa-long-arrow-left">&nbsp;</span>{{ __('Back') }}</a></div>
 					</div>
             	</div>
 
