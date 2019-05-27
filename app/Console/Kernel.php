fb382a46f1648060b347b276675c5533461a4fb8
Michal Voců <michal.vocu@gmail.com>
2019-05-27 17:52:43 +0200

    work

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new SynchronizeExtSources())->daily();
+        $schedule->job(new SynchronizeExtSources())->hourly();
     }
 
     /**
