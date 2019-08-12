6dcebc989f408d3c1dabe3e8ef5427586db80946
Michal Voců <michal.vocu@gmail.com>
2019-08-12 10:35:46 +0200

    log exceptions during sync

--- app/Jobs/SynchronizeExtSource.php
+++ app/Jobs/SynchronizeExtSource.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\Models\Database\ExtSource;
@@ -33,7 +34,11 @@ class SynchronizeExtSource implements ShouldQueue
      */
     public function handle()
     {
+        try {
             $s_mgr = resolve('App\Interfaces\SynchronizationManager');
             $s_mgr->synchronizeExtSource($this->ext_source);
+        } catch (\Exception $e) {
+            Log::error('Error synchronizing ext source', ['source' => $this->ext_source, 'exception' => $e ]);
+        }
     }
 }
