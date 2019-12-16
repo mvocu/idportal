4e6d66af843ab6b7e987602e504dcbcd64fa3629
Michal Voců <michal.vocu@gmail.com>
2019-12-16 18:18:32 +0100

    ldap sync job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs\SynchronizeExtSources;
+use App\Jobs\SynchronizeLdap;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new SynchronizeExtSources())->everyTenMinutes();
+        $schedule->job(new SynchronizeLdap())->everyThirtyMinutes();
     }
 
     /**
