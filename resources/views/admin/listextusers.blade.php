deb3e1d8b3bd84fe1dce8fbc20af1852badd6a08
Michal Vocu <michal@oinochos.mestouvaly.local>
2020-03-05 09:35:06 +0100

    work

--- resources/views/admin/listextusers.blade.php
+++ resources/views/admin/listextusers.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-10">
+        <div class="col-md-12">
             <div class="panel">
 				<div class="panel-body shadow-sm">
 					<form method="POST" action="{{ route('admin.userext.list.search') }}" aria-label="{{ __('Search external users') }}">
