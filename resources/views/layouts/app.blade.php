82409a3ba6ba60a7213bac7ccb3b28164520a415
Michal Voců <michal.vocu@gmail.com>
2020-08-04 13:18:56 +0200

    work

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 	@stack(config('tableView.dataTable.js.stack_name'))
-
+	@stack('app.scripts')
+	
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
