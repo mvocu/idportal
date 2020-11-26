8b927f4a1c519e1dd66582edff0688906d152fa7
Michal Voců <michal.vocu@gmail.com>
2020-11-26 11:33:00 +0100

    fix

--- resources/views/auth/passwords/change.blade.php
+++ resources/views/auth/passwords/change.blade.php
@@ -41,7 +41,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Change password') }}
                                 </button>
