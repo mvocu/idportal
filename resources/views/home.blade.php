3ce7e60cff2dbada58d4f49ec7dab67bca277b79
Michal Vocu <michal@oinochos.mestouvaly.local>
2021-01-11 16:58:38 +0100

    local changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,11 +89,9 @@
                 </div>
 
             	<div class="panel-footer">
-<!--
 					<div class="row">
 						<div class="col-xs-4"><a href="{{ route('password.change') }}" class="btn btn-primary btn-block">{{ __('Change password') }}</a></div>
 					</div>
--->
             	</div>
 
             </div>
