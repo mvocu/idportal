788eaa0e7c7bee2fec2968636ccb0c1d89678e2c
Michal Voců <michal.vocu@gmail.com>
2021-08-27 17:33:49 +0200

    add trait

--- app/Ldap/User.php
+++ app/Ldap/User.php
@@ -4,10 +4,12 @@ namespace App\Ldap;
 
 use LdapRecord\Models\FreeIPA\User as Model;
 use STS\SocialiteAuth\Contracts\SocialiteAuthenticatable;
+use Illuminate\Auth\Authenticatable;
 
 class User extends Model implements SocialiteAuthenticatable
 {
-
+    use Authenticatable;
+    
 	public function getSocialiteIdentifierName()
 	{
     		return 'email';
