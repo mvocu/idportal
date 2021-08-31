9fa0e87e68608d53dc6fa3b9ba71ac42dff84591
Michal Voců <michal.vocu@gmail.com>
2021-08-31 15:57:19 +0200

    work

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', [
+            'aduser' => Auth::user(), 
+            'oidcuser' => Socialite::driver(config('socialite-auth.driver'))->user()::guard('socialite')->getUser() 
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,55 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-10">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
+        </div>
+    </div>
 
-                    {{ __('You are logged in!') }}
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">{{ __('Google Workspace Account') }}</div>
+
+                <div class="card-body">
+
                 </div>
+
             </div>
         </div>
     </div>
+
+	<div class="row justify-content-center mt-5">
+		<div class="col-md-10">
+			<div class="card">
+				<div class="card-header">{{ __('AD Account information') }}</div>
+				<div class="card-body">
+					<div class="row mt-1">
+						<div class="col-4 bg-light">{{ __('Username') }}</div>
+						<div class="col-7">{{ $aduser->getFirstAttribute('samaccountname') }}</div>
+					</div>
+					<div class="row mt-1">
+						<div class="col-4 bg-light">{{ __('Last logon') }}</div>
+						<div class="col-7">{{ $aduser->getFirstAttribute('lastlogon') }}</div>
+					</div>
+					<div class="row mt-1">
+						<div class="col-4 bg-light">{{ __('Last password change') }}</div>
+						<div class="col-7">{{ $aduser->getFirstAttribute('pwdlastset') }}</div>
+					</div>
+					<div class="row mt-1">
+						<div class="col-4 bg-light">{{ __('Account expires') }}</div>
+						<div class="col-7">{{ $aduser->getFirstAttribute('accountexpires') }}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    
 </div>
 @endsection
