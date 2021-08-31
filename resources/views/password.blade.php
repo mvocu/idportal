34b2f6999376dba379be9bcad1b6cdda4aa27c2c
Michal Voců <michal.vocu@gmail.com>
2021-08-31 15:09:08 +0200

    work

--- resources/views/password.blade.php
+++ resources/views/password.blade.php
@@ -8,15 +8,10 @@
 				<div class="card-header">{{ __('Set New Password') }}</div>
 				<div class="card-body">
 
-					<h4 class="card-title">{{ __('Please enter new password') }}</h4>
-
 					<div class="row no-gutters">
-						<div class="col-md-4">
-							<p class="card-text">Zadejte heslo, které budete používat pro
-								přihlášení k počítačům na ZŠ Třebotov.</p>
-						</div>
+						<div class="col-md-7">
+							<h4 class="card-title text-center mb-4">{{ __('Please enter new password') }}</h4>
 
-						<div class="col-md-8">
 							<form method="POST" action="{{ route('adpassword.set') }}">
 								@csrf
 
@@ -25,7 +20,7 @@
 										class="col-md-4 col-form-label text-md-right">{{
 										__('Password') }}</label>
 
-									<div class="col-md-6">
+									<div class="col-md-7">
 										<input id="password" type="password"
 											class="form-control @error('password') is-invalid @enderror"
 											name="password" required autocomplete="current-password">
@@ -40,7 +35,7 @@
 									<label for="password-confirm"
 										class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
-									<div class="col-md-6">
+									<div class="col-md-7">
 										<input id="password-confirm" type="password"
 											class="form-control" name="password_confirmation" required
 											autocomplete="new-password">
@@ -53,8 +48,12 @@
 									</div>
 								</div>
 							</form>
-
 						</div>
+						<div class="col-md-5 bg-light">
+							<p class="card-text p-3">Zadejte heslo, které budete používat pro
+								přihlášení k počítačům na ZŠ Třebotov.</p>
+						</div>
+
 					</div>
 				</div>
 			</div>
