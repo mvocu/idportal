c1135e9dc3d771e2ddf64ba6471b79ba46e706f8
Michal Voců <michal.vocu@gmail.com>
2021-08-31 14:40:56 +0200

    work

--- resources/views/password.blade.php
+++ resources/views/password.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">{{ __('Set New Password') }}</div>
 
@@ -37,8 +37,7 @@
 
 							<div class="form-group row">
 								<label for="password-confirm"
-									class="col-md-4 col-form-label text-md-right">{{ __('Confirm
-									Password') }}</label>
+									class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
 								<div class="col-md-6">
 									<input id="password-confirm" type="password"
@@ -49,8 +48,7 @@
 
 							<div class="form-group row mb-0">
 								<div class="col-md-8 offset-md-4">
-									<button type="submit" class="btn btn-primary">{{ __('Change
-										Password') }}</button>
+									<button type="submit" class="btn btn-primary">{{ __('Change Password') }}</button>
 								</div>
 							</div>
 							</form>
