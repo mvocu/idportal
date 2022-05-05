cc0a9e82a4c7c52c22b430350f52c0ad77d6af0f
Michal Voců <michal.vocu@gmail.com>
2022-05-05 11:26:07 +0200

    fix after removal of ext idp account

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -69,16 +69,14 @@
 					@if ($account['idp'])
                         <div class="row">
                                 <div class="col-4 text-right">{{ __($account['name']) }}</div>
-                                <div class="col-1">{{ empty($value = $user->getFirstAttribute('employeenumber;x-'.$account['tag'])) ? "" : "ano" }}</div>
-					@if (!empty($user->getFirstAttribute('employeenumber;x-'.$account['tag']))) 
+                                <div class="col-1">{{ !isset($account['user_ext']) ? "" : "ano" }}</div>
+					@if (isset($account['user_ext']))
                                 <div class="col-3">tel. {{ empty($account['phone']) ? "" : $account['phone'] }}</div>
    		                        <div class="col-3">mail {{ empty($account['email']) ? "" : $account['email'] }}</div>
-					@if (isset($account['user_ext']))
 								<div class="col-1">
 									<a href="{{ route('ext.account.remove', [ 'user_ext' =>  $account['user_ext'] ] ) }}" class="pull-right btn btn-sm btn-small btn-danger"><span class="fa fa-remove">&nbsp;</span></a>
 									<a href="{{ route('ext.account.show', [ 'user' => $user->getDatabaseUser(), 'source' => $account_id ] ) }}" class="pull-right btn btn-sm btn-small btn-default"><span class="fa fa-search-plus">&nbsp;</span></a>
 								</div>
-					@endif								
 					@else
 								<div class="col-7"><a href="{{ route('register.eidp', [ 'client' => $account['name'] ] ) }}" class="btn btn-social"><span class="fa fa-openid fa-{{ $account['name'] }}">&nbsp;</span>{{ $account['name'] }} - {{ __("Add identity") }} </a></div>
 					@endif
