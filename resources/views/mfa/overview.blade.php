de3eed3a052f4042531802f7c5e0b23662cdf541
Michal Voců <michal.vocu@gmail.com>
2023-01-10 06:49:42 +0100

    view changes

--- resources/views/mfa/overview.blade.php
+++ resources/views/mfa/overview.blade.php
@@ -15,47 +15,48 @@
 	</div>
 	<div class="card mt-4">
 		<ul class="list-group">
-			<li class="list-group-item d-flex flex-row justify-content-between p-3">
-				<div class="text-center" style="width: 64px"><i class="fa fa-mobile-alt fa-3x"></i></div>
-				<div class="flex-grow-1 px-4">
+			<li class="list-group-item d-flex flex-row justify-content-between p-3 align-items-start flex-wrap">
+				<div class="text-center pb-2" style="width: 64px"><i class="fa fa-mobile-alt fa-3x"></i></div>
+				<div class="flex-grow-1 ps-2">
+					<a class="stretch-link btn btn-light btn-lg float-end" href="{{ route('mfa.gauth') }}" role="button"><i class="fa fa-angle-right"></i> </a>
 					<h5>{{ __('Mobile phone') }}</h5>
 					<p>{{ __('List of devices you can use to obtain confirmation code.') }}</p>
-					<div class="container">
-						@if ( !empty($gauth) )
-							@foreach ($gauth as $device)
-							<div class="row align-items-center">
-								<div class="col-1"><i class="bi bi-phone fs-2"></i></div>
-								<div class="col-4 fw-bold">{{ $device->getName() }}</div>
-								<div class="col-7">{{ __('Registered at') }} {{ $device->getRegistrationDate() }} </div>
-							</div> 
-							@endforeach
-						@endif
-					</div>
 				</div>
-				<a class="stretch-link btn btn-light btn-lg" href="{{ route('mfa.gauth') }}" role="button"><i class="fa fa-angle-right"></i> </a>
+				<div class="container offset-sm-1">
+					@if ( !empty($gauth) )
+						@foreach ($gauth as $device)
+						<div class="row align-items-center">
+							<div class="col-sm-1"><i class="bi bi-phone fs-2"></i></div>
+							<div class="col-sm-4 fw-bold">{{ $device->getName() }}</div>
+							<div class="col-sm-7">{{ __('Registered at') }} {{ $device->getRegistrationDate() }} </div>
+						</div> 
+						@endforeach
+					@endif
+				</div>
 			</li>
-			<li class="list-group-item  d-flex flex-row justify-content-between p-3">
-				<div class="text-center" style="width: 64px"><i class="fa fa-key fa-2x"></i></div>
-				<div class="flex-grow-1 px-4">
+			<li class="list-group-item  d-flex flex-row justify-content-between p-3 align-items-start flex-wrap">
+				<div class="text-center pb-2" style="width: 64px"><i class="fa fa-key fa-2x"></i></div>
+				<div class="flex-grow-1 ps-2">
+					<a class="stretch-link btn btn-light btn-lg float-end" href="{{ route('mfa.webauthn') }}" role="button"><i class="fa fa-angle-right"></i> </a>
 					<h5>{{ __('Security key') }}</h5>
 					<p>{{ __('You have registered the following security keys.') }}</p>
-					<div class="container">
-						@if ( !empty($webauthn) )
-							@foreach ($webauthn as $device)
-							<div class="row align-items-center">
-								<div class="col-1"><i class="bi bi-usb-drive fs-2"></i></div>
-								<div class="col-4 fw-bold">{{ $device->getName() }}</div>
-								<div class="col-7">{{ __('Registered at') }} {{ $device->getRegistrationDate() }} </div>
-							</div> 
-							@endforeach
-						@endif
-					</div>
 				</div>
-				<a class="stretch-link btn btn-light btn-lg" href="{{ route('mfa.webauthn') }}" role="button"><i class="fa fa-angle-right"></i> </a>
+				<div class="container offset-sm-1"">
+					@if ( !empty($webauthn) )
+						@foreach ($webauthn as $device)
+						<div class="row align-items-center">
+							<div class="col-sm-1"><i class="bi bi-usb-drive fs-2"></i></div>
+							<div class="col-sm-4 fw-bold">{{ $device->getName() }}</div>
+							<div class="col-sm-7">{{ __('Registered at') }} {{ $device->getRegistrationDate() }} </div>
+						</div> 
+						@endforeach
+					@endif
+				</div>
 			</li>
 			<li class="list-group-item  d-flex flex-row justify-content-between p-3">
-				<div class="text-center"  style="width: 64px"><i class="fa fa-envelope fa-2x"></i></div>
-				<div class="flex-grow-1 px-4">
+				<div class="text-center pb-2"  style="width: 64px"><i class="fa fa-envelope fa-2x"></i></div>
+				<div class="flex-grow-1 ps-2">
+					<a class="stretch-link btn btn-light btn-lg float-end" href="{{ route('mfa.sms') }}" role="button"><i class="fa fa-angle-right"></i> </a>
 					<h5>{{ __('SMS message') }}</h5>
 					@if (!empty($sms))
 					<h6 class="fw-bold">{{ $sms }}</h6>
@@ -63,7 +64,6 @@
 					@else 
 					@endif
 				</div>
-				<a class="stretch-link btn btn-light btn-lg" href="{{ route('mfa.sms') }}" role="button"><i class="fa fa-angle-right"></i> </a>
 			</li>
 		</ul>
 	</div>
