98387ebedc843a958226bb36effcc4c272fbfe34
Michal Voců <michal.vocu@gmail.com>
2023-06-20 15:21:13 +0200

    initial external identity app

---
+++ resources/views/ext/overview.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="d-flex flex-column col-lg-7" style="max-width: 720px">	
+	<div class="card mt-5">
+		<div class="card-body">
+			<h4 class="card-title">{{ __('External identities') }}</h4>
+			<p class="mb-2">{{ __("This application is still being developed. Please come back later.") }}</p>
+		</div> 
+	</div>
+
+</div>
+
+@endsection
