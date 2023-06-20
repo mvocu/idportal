c79d4c68b24c0449fbba456f58d1493dcdafbda3
Michal Vocu <michal@ldapadmin.cuni.cz>
2023-06-20 15:22:54 +0200

    use production cas

--- config/cas.php
+++ config/cas.php
@@ -4,7 +4,7 @@ return [
   
     // GuzzleHttp client parameters
     'connection' => [
-        'base_uri' => 'https://cas1.cuni.cz/cas/actuator',
+        'base_uri' => 'https://cas.cuni.cz/cas/actuator',
         'username' => env('CAS_SERVER_USERNAME', 'cas'),
         'password' => env('CAS_SERVER_PASSWORD', 'changeit'),
     ],
