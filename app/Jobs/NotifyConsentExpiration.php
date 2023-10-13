b7cb9e405f786e403b08b8002ba9982565495e94
Michal Voců <michal.vocu@gmail.com>
2023-10-13 17:46:27 +0200

    added consent expiration warning

---
+++ app/Jobs/NotifyConsentExpiration.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use App\Interfaces\ConsentManager;
+use App\Models\Database\User;
+use App\Notifications\ConsentExpiresNotification;
+use App\Interfaces\LdapConnector;
+
+class NotifyConsentExpiration implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    
+    protected $ldap;
+    protected $consent_mgr;
+
+    public function __construct() 
+    {
+        $this->ldap = resolve(LdapConnector::class);
+        $this->consent_mgr = resolve(ConsentManager::class);
+    }
+    
+    public function handle()
+    {
+        #$users = User::all();
+        $users = [ User::find(2611) ] ;
+        foreach($users as $dbuser) {
+            if($this->consent_mgr->expiresSoon($dbuser)) {
+                $ldapuser = $this->ldap->findUser($dbuser);
+                $user = new \App\User([], $ldapuser->getQuery());
+                $user->setRawAttributes($ldapuser->getAttributes());
+                $user->notify(new ConsentExpiresNotification($user));
+                
+                Log::info('Consent expiration warning sent to ' + $user->getEmail());
+            }
+        }
+    }
+    
+}
+
---
+++ app/Notifications/ConsentExpiresNotification.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
+
+class ConsentExpiresNotification extends Notification
+{
+    use Queueable;
+    
+    private $user;
+    private $token;
+    
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+    
+    /**
+     * Get the notification's channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+    
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+        ->subject(Lang::getFromJson('Uvaly - account consent expiration'))
+        ->line(Lang::getFromJson('Your consent with terms of usage will expire soon and needs to be renewed.') . " " .
+            Lang::getFromJson('Please renew your consent by visiting the link below.'))
+        ->action(Lang::getFromJson('Renew Consent'), url(config('app.url').route('consent.ask')))
+            ->line(Lang::getFromJson('If you do not renew your consent, your account will be disabled.'));
+    }
+    
+}
