cb33e9ed53e8a4c6a9c5399717e439be22f0d5a9
Michal Voců <michal.vocu@gmail.com>
2023-10-13 18:10:01 +0200

    notification existence check

--- app/Jobs/NotifyConsentExpiration.php
+++ app/Jobs/NotifyConsentExpiration.php
@@ -11,6 +11,7 @@ use App\Interfaces\ConsentManager;
 use App\Models\Database\User;
 use App\Notifications\ConsentExpiresNotification;
 use App\Interfaces\LdapConnector;
+use Carbon\Carbon;
 
 class NotifyConsentExpiration implements ShouldQueue
 {
@@ -31,10 +32,16 @@ class NotifyConsentExpiration implements ShouldQueue
         $users = [ User::find(2611) ] ;
         foreach($users as $dbuser) {
             if($this->consent_mgr->expiresSoon($dbuser)) {
+                $requested = new Carbon($dbuser->consent_requested);
+                if($requested->diffInDays(Carbon::now(), true) < 5) {
+                    continue;
+                }
                 $ldapuser = $this->ldap->findUser($dbuser);
                 $user = new \App\User([], $ldapuser->getQuery());
                 $user->setRawAttributes($ldapuser->getAttributes());
                 $user->notify(new ConsentExpiresNotification($user));
+                
+                $this->consent_mgr->setConsentRequested($user, true);
                 
                 Log::info('Consent expiration warning sent to ' + $user->getEmail());
             }
